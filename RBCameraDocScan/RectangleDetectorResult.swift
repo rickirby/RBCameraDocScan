53c8eda292c68bf8dafc776200e077d4cb1cfa3c
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-13 18:28:52 +0700

    add rectangledetectorresult

---
+++ RBCameraDocScan/RectangleDetectorResult.swift
@@ -0,0 +1,15 @@
+//
+//  RectangleDetectorResult.swift
+//  RBCameraDocScan
+//
+//  Created by Ricki Private on 13/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+struct RectangleDetectorResult {
+	
+	let rectangle: Quadrilateral
+	let imageSize: CGSize
+}
