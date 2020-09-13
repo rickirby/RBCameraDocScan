c9b8807b114f891c8c6cc1ec797dafdae79ddcd0
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-13 17:16:24 +0700

    Initial Commit

---
+++ RBCameraDocScan/RBCameraDocScan.h
@@ -0,0 +1,19 @@
+//
+//  RBCameraDocScan.h
+//  RBCameraDocScan
+//
+//  Created by Ricki Private on 13/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RBCameraDocScan.
+FOUNDATION_EXPORT double RBCameraDocScanVersionNumber;
+
+//! Project version string for RBCameraDocScan.
+FOUNDATION_EXPORT const unsigned char RBCameraDocScanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RBCameraDocScan/PublicHeader.h>
+
+
