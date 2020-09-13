4a8f3725b2d58e9443c30a20aea414fb491d1ac7
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-13 18:45:40 +0700

    add addresult

---
+++ RBCameraDocScan/AddResult.swift
@@ -0,0 +1,14 @@
+//
+//  AddResult.swift
+//  RBCameraDocScan
+//
+//  Created by Ricki Private on 13/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+enum AddResult {
+	case showAndAutoScan
+	case showOnly
+}
