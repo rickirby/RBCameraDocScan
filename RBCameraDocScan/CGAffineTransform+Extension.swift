ff140e6712fb172203d22ffce6a09fe045966304
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-13 17:51:48 +0700

    add cgaffinetransform extension

---
+++ RBCameraDocScan/CGAffineTransform+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  CGAffineTransform+Extension.swift
+//  RBCameraDocScan
+//
+//  Created by Ricki Private on 13/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+extension CGAffineTransform {
+	
+	static func scaleTransform(forSize fromSize: CGSize, aspectFillInSize toSize: CGSize) -> CGAffineTransform {
+		let scale = max(toSize.width / fromSize.width, toSize.height / fromSize.height)
+		return CGAffineTransform(scaleX: scale, y: scale)
+	}
+	
+	static func translateTransform(fromCenterOfRect fromRect: CGRect, toCenterOfRect toRect: CGRect) -> CGAffineTransform {
+		let translate = CGPoint(x: toRect.midX - fromRect.midX, y: toRect.midY - fromRect.midY)
+		return CGAffineTransform(translationX: translate.x, y: translate.y)
+	}
+	
+}
