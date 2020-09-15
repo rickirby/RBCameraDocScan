7166df3633e83fa4f6c908a154d24fe5e77fa706
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-15 14:29:43 +0700

    add example project

---
+++ RBCameraDocScanExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RBCameraDocScanExample
+//
+//  Created by Ricki Private on 15/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
