d8bdb8cee38423188f02eb56bc2c541b32302eb8
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-15 16:03:14 +0700

    make RBCameraView class

---
+++ RBCameraDocScan/RBCameraView.swift
@@ -0,0 +1,13 @@
+//
+//  RBCameraView.swift
+//  RBCameraDocScan
+//
+//  Created by Ricki Private on 15/09/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class RBCameraView: UIView {
+	
+}
