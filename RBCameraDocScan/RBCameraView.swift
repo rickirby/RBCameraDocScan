07d36bf6a978eb8f08fd9ce2af1c513e1567e7a6
Ricki Bin Yamin <ricki.bin.yamin16@mhs.ee.its.ac.id>
2020-09-15 16:04:43 +0700

    add init method on view

--- RBCameraDocScan/RBCameraView.swift
+++ RBCameraDocScan/RBCameraView.swift
@@ -10,4 +10,11 @@ import UIKit
 
 class RBCameraView: UIView {
 	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 }
